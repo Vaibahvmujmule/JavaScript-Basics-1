a82923d7d4a1b653d3ff76f354882031e93c8989
vaibhav990 <64162973+vaibhav990@users.noreply.github.com>
2025-10-23 21:29:53 +0530

    Assignment23rdOct

--- script_extended.js
+++ script_extended.js
@@ -4,11 +4,14 @@ console.log("Hi I am learning JS")
 for(let i =1; i<=10; i++){
     console.log(i);
 }
+//Write the same using for each loop
+
 
 //print numbers from 10 to 1
 for(let j=10; j>=1; j--){
     console.log(j);
 }
+//Write the same using for each loop
 // print all the properties of object student using for in loop
 let student = {
     name: "Vaibhav",
@@ -25,3 +28,6 @@ for(let i = 1; i <= 5; i++){
     intial_emp_salary = intial_emp_salary + 100;
     console.log("Increased salary for employee " + intial_emp_salary);
 }
+//write the same using for each loop
+//add break point in line number 28,22,12,5 and click on play button, next button and understand the 
+// difference and the logic 
